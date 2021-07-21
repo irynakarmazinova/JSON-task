76e99c992a7068edabf9d2c0d29f6c1230d6df7c
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-07-22 00:10:51 +0300

    добавила несколько вариантов сортировки и фильтров для джейсона

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import jsonmenu from './my-file.json';
+import jsmenu from './app';
+import './css/styles.css';
+
+// вывести списком этот отсортированный список
+const ulJsEl = document.querySelector('.js-menu');
+
+function createGalleryItems(items) {
+  return items.reduce((acc, { name, image }) => {
+    acc = `${acc} <li class="menu__item">
+      <article class="card">
+        <img
+          src="${image}"
+          alt="${name}"
+          class="card__image"
+        />
+       
+      </article>
+    </li>
+        `;
+    return acc;
+  }, '');
+}
+ulJsEl.insertAdjacentHTML('beforeend', createGalleryItems(jsonmenu));
+
+//  вывести все, что дороже 240 кредитов
+const filterPrice = jsonmenu.filter(({ price }) => {
+  if (price > 240) {
+    console.log(price);
+
+    //   как вывести только те карточки, которые подхоят под условие?
+  }
+});
+
+// вывести все, в чем нет помидоров в ингридиентах
+// const filterNoTomatos = jsonmenu.filter(({ ingredients }) => {
+//   if (!ingredients.includes('Помидоры')) {
+//     console.log(ingredients);
+//   }
+// });
+
+// сортировка цены по возрастанию
+// const sortPriceUp = jsonmenu.sort(function (a, b) {
+//   return a.price - b.price;
+// });
+// console.log(sortPriceUp);
+
+// созтировка имени в алфавитном порядке
+// const sortNameAlphabetically = jsonmenu.sort(function (a, b) {
+//   const nameA = a.name.toLowerCase();
+//   const nameB = b.name.toLowerCase();
+
+//   //сортируем строки по возрастанию
+//   if (nameA < nameB) {
+//     return -1;
+//   }
+//   if (nameA > nameB) {
+//     return 1;
+//   }
+
+//   return 0; // Никакой сортировки
+// });
+// console.log(sortNameAlphabetically);
